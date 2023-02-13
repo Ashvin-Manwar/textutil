79872cadb4d1d1cf854f4a942a4034ac7e1e9210
Ashvin Manwar <manwar.ashvin@gmail.com>
2023-02-13 15:49:04 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
 
+let name="ashvin1"
 function App() {
   return (
+    // <div className="blank">Lovely</div>
+    <>
+    <nav>
+      <li>Home</li>
+      <li>About</li>
+      <li>Contact</li>
+    </nav>
+    <div className="container">/
+    <h1>HEllo {name}</h1>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis quis placeat enim vitae sit pariatur temporibus provident natus exercitationem! Delectus optio possimus dolores sit, nihil eos eaque, voluptatem iste veritatis minima in, omnis repellendus quo nesciunt animi. Incidunt autem est consequuntur sequi assumenda ab ipsa, libero aliquam culpa iste enim hic ipsam, dignissimos vel natus!</p>
+    </div>
+    </>
+
+
+
+
+
+    
+    // <>
+    // {
+    /* <h1>this is first class</h1>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -19,6 +41,8 @@ function App() {
         </a>
       </header>
     </div>
+    </> */
+    // }
   );
 }
 
